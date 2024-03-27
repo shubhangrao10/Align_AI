59a6d8107dc3f7ed9e08175dc1f063789b22379b
shubhangrao10 <shubhangrao10@gmail.com>
2024-03-27 19:12:46 +0530

    Adding files ofthe project

---
+++ lib/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:fitness_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:camera/camera.dart';
+
+import 'main_screen.dart';
+import 'workouts.dart';
+
+class HomeScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  HomeScreen(this.cameras);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    MainScreen(cameras),
+    WorkOutPage(),
+    Image.asset('images/construction.PNG'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        //fixedColor: Colors.white,
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            icon: Icon(LineAwesomeIcons.universal_access),
+            icon: Text('Align'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            icon: Icon(LineAwesomeIcons.burn),
+            label: Text('Workout'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            icon: Icon(LineAwesomeIcons.user),
+            title: Text('Profile'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Color(0xFFFE7C7C),
+        iconSize: 36.0,
+        elevation: 0.0,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
